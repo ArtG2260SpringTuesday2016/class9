5ae9b53f8be21ad76f1e1521fa0de2a07ff42545
Francisco Dias <frandias@gmail.com>
2016-03-15 19:23:58 -0400

    added distance function

--- sketch.js
+++ sketch.js
@@ -2,15 +2,17 @@ var Circle = function(name,x,y,size, fillColor) {
   this.x = x;
   this.y = y;
   this.size = size;
+  this.name = name;
 
   this.renderEllipse = function () {
     fill(fillColor)
     ellipse(this.x, this.y, this.size, this.size)
   }
 
-  // mouseX, mouseY 
-  // dist()
-  // this.x, this.y
+  this.distanceToMouse = function() {
+    console.log(this.name)
+    console.log(dist(mouseX, mouseY, this.x, this.y))
+  }
 }
 
 var myCircle = new Circle("blue", 50,50,50, 'blue');
@@ -26,6 +28,7 @@ function setup() {
 function draw() {
   background(153);
   for (var i = 0; i < myCircles.length; i++) {
-    myCircles[i].renderEllipse()
+    myCircles[i].renderEllipse();
+    myCircles[i].distanceToMouse();
   }
 }
