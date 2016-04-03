ba4469d08aa66c5b5eac5c5037460cdbdad209a4
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-04-03 18:56:01 -0400

    found file on computer
    Like assignment 10 i was never able to get each hover statement to work
    by itself.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,32 @@
-var Circle = function(name,x,y,size, fillColor) {
-  this.x = x;
-  this.y = y;
-  this.size = size;
-  this.name = name;
-
-  this.renderEllipse = function () {
-    fill(fillColor)
-    ellipse(this.x, this.y, this.size, this.size)
-  }
-
-  this.distanceToMouse = function() {
-    return dist(mouseX, mouseY, this.x, this.y);
-  }
+function setup (){
+  createCanvas (800, 800)
+  
 }
 
-var myCircle = new Circle("blue", 50,50,50, 'blue');
-var myOtherCircle = new Circle("red", 200,200,20, 'red');
 
-var myCircles = [myCircle, myOtherCircle];
-
-
-function setup() {
-  createCanvas(600, 500);
-}
-
-function draw() {
-  background(153);
-  for (var i = 0; i < myCircles.length; i++) {
-    myCircles[i].renderEllipse();
-    if (i > 0) {
-        if (myCircles[0].distanceToMouse() < myCircles[1].distanceToMouse()){
-          console.log(myCircles[0].name)
-        } else {
-          console.log(myCircles[1].name)
-        }
-    }
-
+function draw(){
+  background('gray');
+  var mouseover = dist(mouseX, mouseY, 100, 100); 
+  var mouseover1 = dist(mouseX, mouseY, 300, 100); 
+  var mouseover2 = dist(mouseX, mouseY, 300, 300); 
+  var mouseover3 = dist(mouseX, mouseY, 100, 300); 
+  
+  if(mouseover < 50)
+  {
+    MouseSquare = true;
+  }else
+    {MouseSquare = false;
   }
+  
+   if(MouseSquare == true)
+  {
+    fill('yellow');
+  }else
+    {fill('orange'); 
+  }
+
+   rect(100, 100, 100, 100);
+   rect(300, 100, 100, 100);
+   rect(300, 300, 100, 100);
+   rect(100, 300, 100, 100);
 }
