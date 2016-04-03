0110015b7bfb4e024d0902fb3e11dab83a97b376
AnthonyBrandao <Brandao.a@husky.neu.edu>
2016-04-03 18:56:34 -0400

    hover time

--- sketch.js
+++ sketch.js
@@ -1,40 +1,32 @@
-var Circle = function(name,x,y,size, fillColor) {
-  this.x = x;
-  this.y = y;
-  this.size = size;
-  this.name = name;
-
-  this.renderEllipse = function () {
-    fill(fillColor)
-    ellipse(this.x, this.y, this.size, this.size)
-  }
-
-  this.distanceToMouse = function() {
-    return dist(mouseX, mouseY, this.x, this.y);
-  }
+function setup (){
+  createCanvas (500, 500)
 }
 
-var myCircle = new Circle("blue", 50,50,50, 'blue');
-var myOtherCircle = new Circle("red", 200,200,20, 'red');
 
-var myCircles = [myCircle, myOtherCircle];
-
-
-function setup() {
-  createCanvas(600, 500);
-}
-
-function draw() {
-  background(153);
-  for (var i = 0; i < myCircles.length; i++) {
-    myCircles[i].renderEllipse();
-    if (i > 0) {
-        if (myCircles[0].distanceToMouse() < myCircles[1].distanceToMouse()){
-          console.log(myCircles[0].name)
-        } else {
-          console.log(myCircles[1].name)
-        }
-    }
-
+function draw(){
+  background(50)
+  
+  var distance = dist(mouseX, mouseY, 200, 200); 
+  
+  if(distance < 50)
+  {
+    isOverCircle = true;
+  } else {
+    isOverCircle = false;
   }
+  
+   if(isOverCircle == true)
+  {
+    fill(random(1-256));
+    cursor(HAND);
+    text ('YYYEEEAAA BBOOOOYYYY!!!!!!!', 250, 250)
+  } else {
+    fill(100); 
+    cursor(ARROW);
+  
+  }
+   ellipse(200, 200, 100, 100);
+  
+ 
+  
 }
